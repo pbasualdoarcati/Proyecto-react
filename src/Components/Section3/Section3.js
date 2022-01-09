351d896df017b0d7d7e0be45384a2ab5769d36ae
pbasualdoarcati <pbasualdoarcati@hotmail.com>
2022-01-09 15:40:04 -0300

    Modificacion de class por className en section2.js y section3.js y cambio de orden en index.js de scss

--- src/Components/Section3/Section3.js
+++ src/Components/Section3/Section3.js
@@ -3,37 +3,37 @@ import React from 'react'
 const Section3 = () => {
     return (
 /*Background degree, cuadrado en el centro con imagen y rectangulo buttom right con carrusel*/
-        <article class="section3 container-fluid" id="products">
-            <div class="section3__casa">
-                <div class="section3__recuardoNaranja">
-                    <div id="segundo_carousel" class="carousel slide section3_carousel" data-bs-ride="carousel">
-                        <div class="carousel-inner section3_carousel_texto">
-                            <div class="carousel-item active">
-                                <p class="segundo_carousel_texto1">Sed ut perspiciatis.</p>
-                                <h3 class="segundo_carousel_titulo">Nemo Enim</h3>
-                                <p class="segundo_carousel_texto2">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam.</p>
+        <article className="section3 container-fluid" id="products">
+            <div className="section3__casa">
+                <div className="section3__recuardoNaranja">
+                    <div id="segundo_carousel" className="carousel slide section3_carousel" data-bs-ride="carousel">
+                        <div className="carousel-inner section3_carousel_texto">
+                            <div className="carousel-item active">
+                                <p className="segundo_carousel_texto1">Sed ut perspiciatis.</p>
+                                <h3 className="segundo_carousel_titulo">Nemo Enim</h3>
+                                <p className="segundo_carousel_texto2">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam.</p>
                             </div>
-                            <div class="carousel-item">
-                                <p class="segundo_carousel_texto1">Sed ut perspiciatis.</p>
-                                <h3 class="segundo_carousel_titulo">Nemo Enim</h3>
-                                <p class="segundo_carousel_texto2">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam.</p>
+                            <div className="carousel-item">
+                                <p className="segundo_carousel_texto1">Sed ut perspiciatis.</p>
+                                <h3 className="segundo_carousel_titulo">Nemo Enim</h3>
+                                <p className="segundo_carousel_texto2">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam.</p>
                             </div>
-                            <div class="carousel-item">
-                                <p class="segundo_carousel_texto1">Sed ut perspiciatis.</p>
-                                <h3 class="segundo_carousel_titulo">Nemo Enim</h3>
-                                <p class="segundo_carousel_texto2">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam.</p>
+                            <div className="carousel-item">
+                                <p className="segundo_carousel_texto1">Sed ut perspiciatis.</p>
+                                <h3 className="segundo_carousel_titulo">Nemo Enim</h3>
+                                <p className="segundo_carousel_texto2">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam.</p>
                             </div>
                         </div>
-                        <div class="section3_botones">
-                            <button class="carousel-control-prev section3_botones1" type="button" data-bs-target="#segundo_carousel"
+                        <div className="section3_botones">
+                            <button className="carousel-control-prev section3_botones1" type="button" data-bs-target="#segundo_carousel"
                                 data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="false"></span>
-                                <span class="visually-hidden">Anterior</span>
+                                <span className="carousel-control-prev-icon" aria-hidden="false"></span>
+                                <span className="visually-hidden">Anterior</span>
                             </button>
-                            <button class="carousel-control-next section3_botones2" type="button" data-bs-target="#segundo_carousel"
+                            <button className="carousel-control-next section3_botones2" type="button" data-bs-target="#segundo_carousel"
                                 data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Siguiente</span>
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Siguiente</span>
                             </button>
                         </div>
                     </div>
