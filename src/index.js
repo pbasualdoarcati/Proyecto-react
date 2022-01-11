d57328765082c35f9badd4bca7c0c83d5cf11616
pbasualdoarcati <pbasualdoarcati@hotmail.com>
2022-01-10 21:29:10 -0300

    Creacion de carpeta images dentro de Assets, cargado las imagenes en section4 y section3, y puesto como ultima importación section4.scss anterior a bootstrap porque no aplica los cambios de variables

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.scss';
 import './Components/Section2/section2.scss';
 import './Components/Section3/section3.scss';
-import './Components/Section4/section4.scss';
 import './Components/Section5/section5.scss';
+import './Components/Section4/section4.scss';
 import 'bootstrap/dist/js/bootstrap.bundle';
+
 
 ReactDOM.render(
   <React.StrictMode>
