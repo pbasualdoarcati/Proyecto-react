838474fc4d740f8107ac23b38039b7101aef783e
pbasualdoarcati <pbasualdoarcati@hotmail.com>
2022-01-17 18:02:03 -0300

    Error en el nombre de importacion de scss section1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App'
 import reportWebVitals from './reportWebVitals';
 import './index.scss';
-import './Components/Section1/section1.scss';
+import './Components/Section1/Section1.scss';
 import './Components/Section2/section2.scss';
 import './Components/Section3/section3.scss';
 import './Components/Section5/section5.scss';
