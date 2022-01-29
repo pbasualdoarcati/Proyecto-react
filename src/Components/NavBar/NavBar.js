b5458abd5a62b852342ea1fc247ec61fa1472e48
Pablo Salvagni <psalvagni@gmail.com>
2022-01-29 17:23:28 -0300

    agregado de menu hamburgesa en navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,17 +1,64 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 
 function NavBar (props){
+
+    const [click, setClick] = useState('');
+
+    // let menuHamburger = false;
+
+    const showHamburger = () =>{
+      
+      
+      if(click === '' ) {
+        setClick('is-active')
+        console.log('click: ', click)
+       }
+      else {
+        setClick('')
+        console.log('click: ', click)
+      }
+
+      // d.querySelector(panel).classList.toggle("is-active");
+      // d.querySelector(panelButton).classList.toggle("is-active");
+    
+    }
+
     return(
         <div className="container-fluid contenedor__menu">
             <header className='container-fluid header-container'>
+
+                <aside class={`nav__menuHamburger  ${click}`}>
+                        <button className="nav__menuHamburger--close" type="button" onClick={ () => showHamburger () }>
+                            X
+                        </button>
+                        <ul className="nav__menuHamburgerNav">
+                            <li className="nav__menuHamburger--item">
+                                <a href="#home" className="nav-link" aria-current="page" onClick={ () => showHamburger () }>Home</a>
+                            </li>
+                            <li className="nav__menuHamburger--item">
+                                <a href="#about" className="nav-link" onClick={ () => showHamburger () }>About</a>
+                            </li>
+                            <li className="nav__menuHamburger--item">
+                                <a href="#products" className="nav-link" onClick={ () => showHamburger () }>Products</a>
+                            </li>
+                            <li className="nav__menuHamburger--item">
+                                <a href="#corredor" className="nav-link" onClick={ () => showHamburger () }>Services</a>
+                            </li>
+                            <li className="nav__menuHamburger--item">
+                                <a href="#contact" className="nav-link" onClick={ () => showHamburger () }>Contact</a>
+                            </li>
+                        </ul>
+                </aside>
+                
                 <nav className="navbar navbar-expand-lg navbar-light bg-transparent nav__menu" >
 
                     <div className="container top_logo">
                         <img src="./images/top-logo.png" alt="Logo top 25 Watts " className="d-inline-block align-text-top"></img>
                     </div>
 
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" */}
+                    <button className="navbar-toggler" type="button" onClick={ () => showHamburger () }>
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
